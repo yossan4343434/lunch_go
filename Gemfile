662a08dadceded8bfc727671f514cede2e895225
Yoshiyuki Kuga <yossan4343434@gmai.com>
2017-08-26 20:09:29 +0900

    Add account activation

--- Gemfile
+++ Gemfile
@@ -6,7 +6,6 @@ git_source(:github) do |repo_name|
 end
 
 gem 'rails', '~> 5.1.2'
-gem 'sqlite3'
 gem 'puma', '~> 3.7'
 gem 'sass-rails', '~> 5.0'
 gem 'uglifier', '>= 1.3.0'
@@ -27,6 +26,7 @@ gem 'bootstrap-will_paginate', '1.0.0'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
+  gem 'sqlite3', '1.3.13'
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
   # Adds support for Capybara system testing and selenium driver
   gem 'capybara', '~> 2.13'
@@ -41,6 +41,10 @@ group :development do
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
+end
+
+group :production do
+  gem 'pg', '0.20.0'
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -2,6 +2,10 @@ require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
 
+  def setup
+    ActionMailer::Base.deliveries.clear
+  end
+
   test "invalid signup information" do
     get signup_path
     assert_no_difference 'User.count' do
@@ -10,11 +14,27 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_template 'users/new'
   end
 
-  test "valid signup information" do
+  test "valid signup information with account activation" do
     get signup_path
-    assert_difference 'User.count' do
+    assert_difference 'User.count', 1 do
       post users_path, params: { user: { name: "Example User", email: "user@example.com", password: "password", password_confirmation: "password" } }
     end
+    assert_equal 1, ActionMailer::Base.deliveries.size
+    user = assigns(:user)
+    assert_not user.activated?
+
+    log_in_as(user)
+    assert_not is_logged_in?
+
+    get edit_account_activation_path("Invalid token", email: user.email)
+    assert_not is_logged_in?
+
+    get edit_account_activation_path(user.activation_token, email: 'wrong@gmail.com')
+    assert_not is_logged_in?
+
+    get edit_account_activation_path(user.activation_token, email: user.email)
+    assert user.reload.activated?
+
     follow_redirect!
     assert_template 'users/show'
     assert is_logged_in?

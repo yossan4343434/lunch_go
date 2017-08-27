5d627976e6359054eb9a079791d1548d5b0d8600
Yoshiyuki Kuga <yossan4343434@gmai.com>
2017-08-27 16:48:57 +0900

    fix initializers

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -4,8 +4,8 @@ if Rails.env.production?
       :provider => 'AWS',
       :region => ENV['S3_REGION'],
       :aws_access_key_id => ENV['S3_ACCESS_KEY'],
-      :aws_secret_accesskey => ENV['S3_SECRET_KEY']
+      :aws_secret_access_key => ENV['S3_SECRET_KEY']
     }
-    cofig.fog_directory = ENV['S3_BUCKET']
+    config.fog_directory = ENV['S3_BUCKET']
   end
 end

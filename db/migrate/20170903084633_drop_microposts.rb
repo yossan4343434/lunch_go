3d63e828e85d91253641551c091eb06afdc11bf6
Yoshiyuki Kuga <yossan4343434@gmai.com>
2017-09-03 19:17:05 +0900

    remove migration files

--- db/migrate/20170903084633_drop_microposts.rb
+++
@@ -1,5 +0,0 @@
-class DropMicroposts < ActiveRecord::Migration[5.1]
-  def change
-    drop_table :microposts
-  end
-end
--- db/migrate/20170903084713_drop_relationships.rb
+++
@@ -1,5 +0,0 @@
-class DropRelationships < ActiveRecord::Migration[5.1]
-  def change
-    drop_table :relationships
-  end
-end
